036a8c0997f72895cc4290ffeff21b7a9ec73264
Taureon <45183108+Taureon@users.noreply.github.com>
2024-03-10 11:42:11 +0100

    Initial commit

---
+++ src/main/java/net/taureon/tauexample/TauExampleClient.java
@@ -0,0 +1,10 @@
+package net.taureon.tauexample;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TauExampleClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
